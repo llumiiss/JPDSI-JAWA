8d47593ad686f27f5cc8309af8ac5eaa8160e3c7
llumiiss <mobi.litosh@gmail.com>
2025-11-19 23:44:50 +0100

    Init: add litoshcalc project

--- litoshcalc/src/main/java/com/mycompany/litoshcalc/servlets/CreditCalculatorServlet.java
+++ litoshcalc/src/main/java/com/mycompany/litoshcalc/servlets/CreditCalculatorServlet.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.annotation.WebServlet;
 
-@WebServlet("/result")  // Mapowanie do URL "/result"
+@WebServlet("/result")
 public class CreditCalculatorServlet extends HttpServlet {
 
     // Obsługuje żądanie POST
